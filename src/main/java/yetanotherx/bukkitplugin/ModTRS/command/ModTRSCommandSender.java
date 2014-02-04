c5b54f885a75f158efa40a5370b11f53ea1391ab
GoalieGuy6 <goalieguy6@gmail.com>
2014-02-03 20:03:25 -0500

    Update ModTRSCommandSender

--- src/main/java/yetanotherx/bukkitplugin/ModTRS/command/ModTRSCommandSender.java
+++ src/main/java/yetanotherx/bukkitplugin/ModTRS/command/ModTRSCommandSender.java
@@ -1,46 +1,35 @@
 package yetanotherx.bukkitplugin.ModTRS.command;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import yetanotherx.bukkitplugin.ModTRS.util.ModTRSPermissions;
 
 public class ModTRSCommandSender {
 
-    private ConsoleCommandSender console;
-    private Player player;
+    private final CommandSender sender;
 
     public ModTRSCommandSender(CommandSender sender) {
-        if (sender instanceof Player) {
-            player = (Player) sender;
-        } else if (sender instanceof ConsoleCommandSender) {
-            console = (ConsoleCommandSender) sender;
-        }
+        this.sender = sender;
     }
 
     public boolean isValidSender() {
-        return console != null || player != null;
+        return sender != null;
     }
 
     public void sendMessage(String string) {
-        if (console != null) {
-            console.sendMessage(string);
-        } else if (player != null) {
-            player.sendMessage(string);
-        }
+        sender.sendMessage(string);
     }
 
     public boolean hasPerm(String permission, boolean restricted) {
-        if (this.console != null) {
-            return true;
+        if (restricted) {
+            return sender.hasPermission(permission);
         }
 
-        return ModTRSPermissions.has(this.player, permission, restricted);
-
+        return true;
     }
 
     public boolean hasPerm(String permission) {
@@ -48,44 +37,44 @@ public class ModTRSCommandSender {
     }
 
     public Server getServer() {
-        if (console != null) {
-            return console.getServer();
-        } else if (player != null) {
-            return player.getServer();
-        }
-        return null;
+        return sender.getServer();
     }
 
     public String getName() {
-        if (console != null) {
+        if (sender instanceof ConsoleCommandSender) {
             return "console";
-        } else if (player != null) {
-            return ChatColor.stripColor(player.getName());
+        } else {
+            return sender.getName();
         }
-        return "";
     }
 
     public World getWorld() {
-        if (console != null) {
+        if (sender instanceof ConsoleCommandSender) {
             return this.getServer().getWorlds().get(0);
-        } else if (player != null) {
-            return player.getWorld();
+        } else if (sender instanceof BlockCommandSender) {
+            return ((BlockCommandSender) sender).getBlock().getWorld();
+        } else if (sender instanceof Player) {
+            return ((Player) sender).getWorld();
         }
+
         return null;
     }
 
     public Location getLocation() {
-        if (console != null) {
+        if (sender instanceof ConsoleCommandSender) {
             return new Location(this.getServer().getWorlds().get(0), 0, 0, 0);
-        } else if (player != null) {
-            return player.getLocation();
+        } else if (sender instanceof BlockCommandSender) {
+            return ((BlockCommandSender) sender).getBlock().getLocation();
+        } else if (sender instanceof Player) {
+            return ((Player) sender).getLocation();
         }
+
         return null;
     }
 
     public void teleport(Location location) {
-        if (player != null) {
-            player.teleport(location);
+        if (sender instanceof Player) {
+            ((Player) sender).teleport(location);
         }
     }
 }
